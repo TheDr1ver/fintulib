47620d28a8f86ade6bd4dd028eafea9eb8d8ecab
Philipp Bodewig <philipp.bodewig@fintu.de>
2018-05-13 17:40:07 +0200

    Add McNemar's test wrapper to metrics

--- fintulib/model/metrics.py
+++ fintulib/model/metrics.py
@@ -1,8 +1,12 @@
 """Metrics to score models not found in the modelling libraries themselves
 """
-import  numpy as np
+import pandas as pd
+import numpy as np
+from statsmodels.stats.contingency_tables import Table, mcnemar
 
 # from  https://www.kaggle.com/dimitrislev/xgboost-in-python-with-rmspe
+
+
 def _rmspe_weights(y):
     w = np.zeros(y.shape, dtype=float)
     ind = y != 0
@@ -10,10 +14,31 @@ def _rmspe_weights(y):
     return w
 
 
-def RMSPE(y,yhat):
+def RMSPE(y, yhat):
     """Root mean squared percentage error"""
     w = _rmspe_weights(y)
-    rmspe = np.sqrt(np.mean( w * (y - yhat)**2 ))
+    rmspe = np.sqrt(np.mean(w * (y - yhat)**2))
     return rmspe
 
 
+def _mcnemar_p_value_cols(df, col1, col2):
+    """Calculate McNemar's test on two columns of a data frame"""
+    table = Table.from_data(df[[col1, col2]])
+    result = mcnemar(table.table)
+    return result.pvalue
+
+
+def mcnemar_p_value(df, sort_columns=True):
+    """Calculate McNemar's test on subsequent columns of a data frame.
+    
+    :param df: The data frame to use.
+    :param sort_columns: If true, columns will be sorted by their name
+    before calculating McNemar's test scores.
+    """
+    if sort_columns:
+        cols = list(df.columns.sort_values())
+    else:
+        cols = list(df.columns)
+    pairs = list(zip(cols, cols[1:]))
+    p_values = [_mcnemar_p_value_cols(df, p[0], p[1]) for p in pairs]
+    return pd.DataFrame(np.array([pairs, p_values]).T, columns=["pair", "p_value"]).set_index("pair")
--- setup.py
+++ setup.py
@@ -4,20 +4,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='fintulib',
-    version='0.0.2',
+    version='0.0.3',
     author="Anselm Schultes, Philipp Bodewig. (C) 2018 Fintu Data Science GmbH.",
     description="A collection of useful code snippets for data wrangling and analysis",
     url='https://github.com/Fintu/fintulib',
     packages=find_packages(exclude=['contrib', 'docs', 'tests', '__pycache__']),
     include_package_data=True,
     install_requires=[
-        "google-cloud-storage == 1.8.0",
-        "dill == 0.2.7.1",
-        "numpy == 1.14.2",
-        "pandas == 0.22.0",
-        "protobuf == 3.0.0",
-        "scikit_learn == 0.19.1",
-        "scipy == 1.1.0",
-        "setuptools == 39.0.1"
+        "google-cloud-storage >= 1.8.0",
+        "dill >= 0.2.7.1",
+        "numpy >= 1.14.2",
+        "pandas >= 0.22.0",
+        "protobuf >= 3.0.0",
+        "scikit_learn >= 0.19.1",
+        "scipy >= 1.1.0",
+        "setuptools >= 39.0.1",
+        "statsmodels >= 0.8.0"
     ]
 )

2cfe6f5fb1d85c26ea2a0cf2bb08d3a49f95174a
Anselm Schultes <anselm.schultes+dev@fintu.de>
2018-05-22 15:26:54 +0200

    apply_on_rows_chunkwise: pass through arguments

--- fintulib/common/pandas.py
+++ fintulib/common/pandas.py
@@ -5,16 +5,17 @@ import numpy as np
 from tqdm import tqdm
 
 
-def apply_on_rows_chunkwise(df, func, rows_per_chunk=300000):
+def apply_on_rows_chunkwise(df, func, rows_per_chunk=300000, **kwargs):
     """ Apply a function over rows of a DataFrame in chunks.
     Useful to reduce memory requirements, especially if those grow non-linearly with row number.
     :df: The DataFrame to rowise apply a chunked function over
     :func: The callable function to apply on each chunk
     :rows_per_chunk: Rows per chunk. Optimize manually, for example by setting \
         such that the operation uses most of the available RAM, but does not swap.
+    :kwargs: Named arguments passed as such to the function func
     """
     df_results_list = []
     # TODO is there an easy way to parallelize this?
     for _, df_chunk in tqdm(df.groupby(np.arange(len(df)) // rows_per_chunk)):
-        df_results_list.append(func(df_chunk))
+        df_results_list.append(func(df_chunk, **kwargs))
     return pd.concat(df_results_list)
